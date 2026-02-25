7d83cc2e961df59eb80ec7f8d247fb54f89a714c
Zach Paden <zach.paden@symetra.com>
2026-02-25 09:16:41 -0500

    feat: deploy frontend

--- database/main.sql
+++ database/main.sql
@@ -12,3 +12,11 @@
 \i products.sql
 \i index_options.sql
 \i riders.sql
+
+CREATE TABLE llm_converstations (
+    id BIGINT BY DEFAULT AS IDENTITY PRIMARY KEY,
+    session_id BIGINT,
+    data JSONB,
+    create_dt TIMESTAMPTZ DEFAULT NOW() NOT NULL,
+    update_dt TIMESTAMPTZ DEFAULT NOW() NOT NULL
+)
